32359a0acf7e73da2122015465fa973e38c1d1ea
joaopachiel <joaopachiel@gmail.com>
2023-08-17 19:47:52 -0300

    build: Instalação e configuração do swagger.

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -1,9 +1,11 @@
+using System;
 using Api.CrossCutting.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -22,6 +24,22 @@ namespace application
             ConfigureService.ConfigureDependencyService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Api com AspNetCore",
+                    Description = "Arquitetura DDD",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "João Pedro Costa Pachiel",
+                        Email = "joaopachiel@gmail.com",
+                        Url = new Uri("https://www.linkedin.com/in/joaopachiel")
+                    },
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +49,14 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api com AspNetCore");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
