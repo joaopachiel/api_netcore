283f1712f14ec84dc9d909b3e22f95c708c1ac9a
joaopachiel <joaopachiel@gmail.com>
2023-09-06 19:31:45 -0300

    build: configurações de injeção de dependência.

--- src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
+++ src/Api.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -13,6 +13,7 @@ namespace Api.CrossCutting.DependencyInjection
         public static void ConfigureDependencyService(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<IUserService, UserService>();
+            serviceCollection.AddTransient<ILoginService, LoginService>();
         }
     }
 }
